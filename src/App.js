08e543e35371fef287c2d22bba34c9922c004254
Matthew Long <matthewlong@matthews-air.lan>
2022-02-26 22:56:37 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import TinderCard from 'react-tinder-card';
+import xml2json from './utils/xml2json';
 
 function App() {
+
+  const [bggData, setbggData] = useState();
+
+  fetch('https://boardgamegeek.com/xmlapi2/collection?username=Sforzando&excludesubtype=boardgameexpansion')
+    .then(response => response.text())
+    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then(data => {
+      var bggData = JSON.parse(xml2json(data, ''))
+      setbggData(bggData);
+    });
+
+  const onSwipe = (direction) => {
+    console.log('You swiped: ' + direction)
+  }
+
+  const onCardLeftScreen = (myIdentifier) => {
+    console.log(myIdentifier + ' left the screen')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']}>Hello, World!</TinderCard>
+      {bggData && bggData.items.item.map((game, key) => {
+        return <img src={game.thumbnail} key={key} />
+      })}
+    </>
+  )
 }
 
 export default App;
