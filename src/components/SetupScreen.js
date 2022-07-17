c929cc8c6a8c68cff014d3edb2bb569f4fd527a1
Matthew Long <mattlong87@gmail.com>
2022-07-17 11:35:19 -0400

    Update SetupScreen.js

--- src/components/SetupScreen.js
+++ src/components/SetupScreen.js
@@ -7,14 +7,28 @@ const Screen = styled.div`
     align-items: center;
 `
 
+const Filter = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+const Label = styled.label`
+    font-size: 20px;
+
+`
+
+const DropDown = styled.select`
+    font-size: 20px;
+`
+
 export default class SetupScreen extends React.Component {
     render() {
       return (
         <Screen>
           <h1>Setup</h1>
-          <div>
-              <label>Number of Players</label>
-              <select>
+          <Filter>
+              <Label>Players</Label>
+              <DropDown>
                   <option>1</option>
                   <option>2</option>
                   <option>3</option>
@@ -23,8 +37,8 @@ export default class SetupScreen extends React.Component {
                   <option>6</option>
                   <option>7</option>
                   <option>8+</option>
-              </select>
-          </div>
+              </DropDown>
+          </Filter>
           <button>Start</button>
         </Screen>
       );
