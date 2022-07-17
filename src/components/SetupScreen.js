5790768f8aca895c39f6288d5713e4416f5a1344
Matthew Long <mattlong87@gmail.com>
2022-07-17 11:07:19 -0400

    added player count option

--- src/components/SetupScreen.js
+++ src/components/SetupScreen.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Screen = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
 
 export default class SetupScreen extends React.Component {
     render() {
       return (
-        <div>
-          <h1>Hello, world!</h1>
-        </div>
+        <Screen>
+          <h1>Setup</h1>
+          <div>
+              <label>Number of Players</label>
+              <select>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                  <option>7</option>
+                  <option>8+</option>
+              </select>
+          </div>
+          <button>Start</button>
+        </Screen>
       );
     }
   }
