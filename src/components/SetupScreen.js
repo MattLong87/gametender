7df5009d56dff688af84740ba217a70ea384d89f
Matthew Long <mattlong87@gmail.com>
2022-07-17 14:31:20 -0400

    more styling for setupscreen

--- src/components/SetupScreen.js
+++ src/components/SetupScreen.js
@@ -5,6 +5,21 @@ const Screen = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
+    background: white;
+    height: 100vh;
+    color: #333;
+`
+
+const Container = styled.div`
+    border-radius: 8px;
+    width: 90vw;
+    min-height: 90vh;
+    padding: 30px;
+    box-sizing: border-box;
+`
+const Title = styled.h1`
+    text-align: center;
 `
 
 const Filter = styled.div`
@@ -14,33 +29,49 @@ const Filter = styled.div`
 
 const Label = styled.label`
     font-size: 20px;
-
 `
 
 const DropDown = styled.select`
     font-size: 20px;
 `
 
+const Button = styled.button`
+    width: 100%;
+    background-color: #08AEEA;
+    background-image: linear-gradient(342deg, #08AEEA 0%, #2AF598 100%);    
+    border: none;
+    color: white;
+    border-radius: 8px;
+    padding: 20px 0;
+    font-size: 20px;
+    font-weight: bold;
+    letter-spacing: 2px;
+    margin-top: 20px;
+    box-shadow: 0 2px 0px 1px hsl(194deg 77% 37%);
+`
+
 export default class SetupScreen extends React.Component {
     render() {
-      return (
-        <Screen>
-          <h1>Setup</h1>
-          <Filter>
-              <Label>Players</Label>
-              <DropDown>
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                  <option>6</option>
-                  <option>7</option>
-                  <option>8+</option>
-              </DropDown>
-          </Filter>
-          <button>Start</button>
-        </Screen>
-      );
+        return (
+            <Screen>
+                <Container>
+                    <Title>Setup</Title>
+                    <Filter>
+                        <Label>Players</Label>
+                        <DropDown>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                            <option>7</option>
+                            <option>8+</option>
+                        </DropDown>
+                    </Filter>
+                    <Button>START</Button>
+                </Container>
+            </Screen>
+        );
     }
-  }
+}
