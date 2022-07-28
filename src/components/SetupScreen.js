8c01869891c7ecb659f697408b96657245e00edf
Matthew Long <mattlong87@gmail.com>
2022-07-27 21:46:48 -0400

    initial styling of game length radio buttons. reworked the jsx to allow for this. used an external stylesheet for convenience.

--- src/components/SetupScreen.js
+++ src/components/SetupScreen.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import './radio-options.css';
 
 const Screen = styled.div`
     display: flex;
@@ -12,9 +13,9 @@ const Screen = styled.div`
 
 const Container = styled.div`
     border-radius: 8px;
-    width: 90vw;
+    width: 100vw;
     min-height: 90vh;
-    padding: 30px;
+    padding: 20px;
     box-sizing: border-box;
 `
 
@@ -30,7 +31,7 @@ const Filter = styled.div`
     display: flex;
     flex-direction: column;
     align-items: flex-start;
-    margin-bottom: 20px;
+    margin-bottom: 40px;
 `
 
 const Label = styled.label`
@@ -60,6 +61,10 @@ const Button = styled.button`
     bottom: 40px;
     left: 5vw;
     width: 90vw;
+    &:active{
+      background-image: linear-gradient(342deg,#08AEEA 0%,#1f7d52 100%);
+      box-shadow: inset 0 2px 0px 1px hsl(194deg 77% 37%);
+    }
 `
 
 const Input = styled.input`
@@ -68,105 +73,117 @@ const Input = styled.input`
     width: 100%;
 `
 
-export default function SetupScreen({createInput}) {
-    let initialFormState = {
-        playercount: "2",
-        playtime: "short"
-      };
+const RadioOption = styled.div``;
 
-      const [formData, setFormData] = useState({ ...initialFormState });
-      const handleChange = ({ target }) => {
-        const value =  target.value;
-        setFormData({
-          ...formData,
-          [target.name]: value,
-        });
-      };
-     
-    
-      const handleSubmit = (event) => {
-        event.preventDefault();
-        createInput(formData);
-        console.log("Submitted:", formData); //Replace...
-        //setFormData({ ...initialFormState }); This will clear the form...
-      };
+export default function SetupScreen({ createInput }) {
+  let initialFormState = {
+    playercount: "2",
+    playtime: "30"
+  };
 
-        return (
-            <Screen>
-                <Container>
-                    <Title>Setup</Title>
-                    <form onSubmit={handleSubmit}>
-                    <Filter>
-                        <Label>BGG Username</Label>
-                        <Input onChange={(e) => console.log(e.target.value)} />
-                    </Filter>
-                    <Filter>
-                    <Label htmlFor="playercount">Players</Label>
-                        <DropDown
-                        id="playercount"
-                        name="playercount"
-                        onChange={handleChange}
-                        value={formData.playercount}>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            <option>6</option>
-                            <option>7</option>
-                            <option>8+</option>
-                        </DropDown>
-                     </Filter>
-                     <Filter>
-                       <Label>Game Length</Label>
-                       <Label htmlFor="short">
-                        Under 30 minutes
-                        <input
-                          id="short"
-                          type="radio"
-                          name="playtime"
-                          onChange={handleChange}
-                          value="30"
-                          checked={formData.playtime === "30"}
-                        />
-                       </Label>
-                       <Label htmlFor="average">
-                        About an hour
-                        <input
-                          id="average"
-                          type="radio"
-                          name="playtime"
-                          onChange={handleChange}
-                          value="60"
-                          checked={formData.playtime === "60"}
-                        />
-                        </Label>
-                        <Label htmlFor="long">
-                        About two hours
-                        <input
-                          id="long"
-                          type="radio"
-                          name="playtime"
-                          onChange={handleChange}
-                          value="120"
-                          checked={formData.playtime === "120"}
-                        />
-                        </Label>
-                        <Label htmlFor="very long">
-                        Over two hours
-                        <input
-                          id="very long"
-                          type="radio"
-                          name="playtime"
-                          onChange={handleChange}
-                          value="240"
-                          checked={formData.playtime === "240"}
-                        />
-                        </Label>
-                      </Filter>
-                    <Button onClick={()=> handleSubmit}>START</Button>
-                    </form>
-                </Container>
-            </Screen>
-        );
-    };
+  const [formData, setFormData] = useState({ ...initialFormState });
+  const handleChange = ({ target }) => {
+    const value = target.value;
+    setFormData({
+      ...formData,
+      [target.name]: value,
+    });
+  };
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createInput(formData);
+    console.log("Submitted:", formData); //Replace...
+    //setFormData({ ...initialFormState }); This will clear the form...
+  };
+
+  return (
+    <Screen>
+      <Container>
+        <Title>Setup</Title>
+        <form onSubmit={handleSubmit}>
+          <Filter>
+            <Label>BGG Username</Label>
+            <Input onChange={(e) => console.log(e.target.value)} />
+          </Filter>
+          <Filter>
+            <Label htmlFor="playercount">Players</Label>
+            <DropDown
+              id="playercount"
+              name="playercount"
+              onChange={handleChange}
+              value={formData.playercount}>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8+</option>
+            </DropDown>
+          </Filter>
+          <Filter>
+            <Label>Game Length</Label>
+            <div className="radio-options">
+              <RadioOption>
+                <input
+                  id="short"
+                  type="radio"
+                  name="playtime"
+                  onChange={handleChange}
+                  value="30"
+                  checked={formData.playtime === "30"}
+                />
+                <Label htmlFor="short">
+                  Short
+                </Label>
+              </RadioOption>
+              <RadioOption>
+                <input
+                  id="average"
+                  type="radio"
+                  name="playtime"
+                  onChange={handleChange}
+                  value="60"
+                  checked={formData.playtime === "60"}
+                />
+                <Label htmlFor="average">
+                  Medium
+                </Label>
+              </RadioOption>
+              <RadioOption>
+                <input
+                  id="long"
+                  type="radio"
+                  name="playtime"
+                  onChange={handleChange}
+                  value="120"
+                  checked={formData.playtime === "120"}
+                />
+                <Label htmlFor="long">
+                  Long
+                </Label>
+              </RadioOption>
+              <RadioOption>
+                <input
+                  id="very long"
+                  type="radio"
+                  name="playtime"
+                  onChange={handleChange}
+                  value="240"
+                  checked={formData.playtime === "240"}
+                />
+                <Label htmlFor="very long">
+                  Very Long
+                </Label>
+              </RadioOption>
+            </div>
+          </Filter>
+          <Button onClick={() => handleSubmit}>START</Button>
+        </form>
+      </Container>
+    </Screen>
+  );
+};
