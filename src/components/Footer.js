7a19f10eff811614baf789632227b53c89706e34
Matthew Long <mattlong87@gmail.com>
2022-09-29 21:13:36 -0400

    initial footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const FooterContainer = styled.div`
+background: #26124a;
+color: white;
+`
+
+export default function Footer() {
+    return <FooterContainer>Made by Matt Long and Ian Finley</FooterContainer>
+}
